9b07a1eff355f4f641862e7c812b0763ffcf0b9a
Tamas Majer <tamas.majer@gmail.com>
2022-10-02 01:23:59 +0200

    fix: don't throw if meta key does not exists

--- src/drivers/http-headers.mjs
+++ src/drivers/http-headers.mjs
@@ -10,17 +10,19 @@ export default defineDriver((opts = {}) => {
       return $fetch(r(key), { method: "HEAD", headers }).then(() => true).catch(() => false);
     },
     async getItem(key) {
-      const value = await $fetch(r(key), { method:"GET", headers });
+      const value = await $fetch(r(key), { method: "GET", headers });
       return value;
     },
     async getMeta(key) {
-      const res = await $fetch.raw(r(key), { method: "HEAD", headers });
-      const _lastModified = res.headers.get("last-modified");
-      let mtime=_lastModified?new Date(_lastModified):undefined;
-      return {
-        status: res.status,
-        mtime
-      };
+      try {
+        const res = await $fetch.raw(r(key), { method: "HEAD", headers });
+        const _lastModified = res.headers.get("last-modified");
+        let mtime = _lastModified ? new Date(_lastModified) : undefined;
+        return { status: 200, mtime };
+      }
+      catch (e) {
+        return { status: 404 };
+      }
     },
     async setItem(key, value) {
       await $fetch(r(key), { method: "PUT", headers, body: stringify(value) });
@@ -29,7 +31,7 @@ export default defineDriver((opts = {}) => {
       await $fetch(r(key), { method: "DELETE", headers });
     },
     async getKeys() {
-      const value = await $fetch(r(""), { method: "GET", headers } );
+      const value = await $fetch(r(""), { method: "GET", headers });
       return Array.isArray(value) ? value : [];
     },
     clear() {
